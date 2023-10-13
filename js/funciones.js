525e9e790be6fbf53e5a46070e748f831741657a
Carolina Leon <sclh2408@gmail.com>
2023-10-12 21:10:31 -0500

    actualizado

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,22 @@
-jQuery(document).ready(listo); 
+jQuery(document).ready(listo);
 
 function listo() {
     /*alert("Hola Mundo");*/
-    jQuery(".hamb").clicK(function(e){
+    jQuery(".hamb").click(function(e) {
         e.preventDefault();
-        jQuery("header .container nav").toggleClass("open");    
-        });
-    }
+        jQuery("header .container nav").toggleClass("open");
+        jQuery(".hamb i").toggleClass("fa-times");
+    });
+
+    jQuery("header .container nav a").click(function() {
+        jQuery("header .container nav").removeClass("open");
+        jQuery(".hamb i").removeClass("fa-times");
+
+        var dev = jQuery(this).attr("href");
+
+        jQuery("html, body").animate({
+            scrollTop: jQuery(dev).offset().top
+        }, 1000);
+    });
+}
+
